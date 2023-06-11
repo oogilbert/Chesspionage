14000a400e8689fac390ef2be113945489f547b9
oogilbert <gilbert.oscar@gmail.com>
2023-06-11 12:34:39 -0500

    Refactor to include color in tree nodes

--- main.py
+++ main.py
@@ -7,10 +7,24 @@ pgns = io.StringIO(requests.get('https://lichess.org/api/games/user/' + name).te
 history = parse_games(pgns, name)
 
 #Todo add a history explorer that doesn't require knowing chess notation
+white = history.children[0]
+black = history.children[1]
+print("Number of games played as white: " + str(white.weight))
+print("Winrate as white:  " + str(100 * white.result / white.weight))
+print("")
+print("Number of games played as black: " + str(black.weight))
+print("Winrate as black:  " + str(100 * black.result / black.weight))
+userColor = input("enter \"White\" to inspect games as white or \"Black\" to inspect games as black: ")
+if userColor == "White":
+    history = white
+elif userColor == "Black":
+    history = black
+else:
+    raise Exception("Error: unrecognized color")
 while history.children != []:
     for node in history.children:
         print("")
-        print("Move: " + node.move)
+        print(node.color + " plays: " + node.move)
         print("Number of games played:  " + str(node.weight))
         print("Winrate:  " + str(100 * node.result / node.weight))
     nextMove = input("Enter next move: ")
--- tree.py
+++ tree.py
@@ -1,22 +1,22 @@
 import chess.pgn
 from fractions import Fraction
 
-#Todo split nodes between player moves and opponent moves
 #Todo combine move transpositions
 class ChessNode:
-    def __init__(self, move):
-        self.weight = 1
+    def __init__(self, move, color, weight = 1):
+        self.weight = weight
         self.move = move
         self.children = []
         self.result = 0
+        self.color = color
         
     #Increments the weight of a child node if it already exists or creates a new child if it doesnt. Returns the child.
-    def add_child(self, newMove):
+    def add_child(self, newMove, color):
         for child in self.children:
-            if child.move == newMove:
+            if child.move == newMove and child.color == color: #Same color check may be needed for combining transpositions
                 child.weight += 1
                 return child
-        newNode = ChessNode(newMove)
+        newNode = ChessNode(newMove, color)
         self.children.append(newNode)
         return newNode
         
@@ -25,17 +25,20 @@ class ChessNode:
 
 #Parse a list of a user's pgns into a tree structure
 def parse_games(pgns, name):
-    treeStart = ChessNode("start")
+    #Add nodes for white and black
+    treeStart = ChessNode("start", "None", 0)
+    treeStart.children.append(ChessNode("White", "None", 0))
+    treeStart.children.append(ChessNode("Black", "None", 0))
     while (game := chess.pgn.read_game(pgns)) is not None:
         resultString = game.headers["Result"].split("-")[0]  #Results strings are formatted as "1/2-1/2", "1-0" etc where the white player is listed first
         result = .5 if resultString == "1/2" else int(resultString)
         
-        #add a node for all games and two child nodes for games where the player is black or white
+        #go back to the start of the tree and invert score if player is playing black
         currentNode = treeStart
         if game.headers["White"] == name:
-            currentNode = treeStart.add_child("White")
+            currentNode = treeStart.add_child("White", "None")
         elif game.headers["Black"] == name:
-            currentNode = treeStart.add_child("Black")
+            currentNode = treeStart.add_child("Black", "None")
             result = 1 - result #invert score when playing as black
         else:
             raise Exception("User was not found in the current game")
@@ -43,7 +46,12 @@ def parse_games(pgns, name):
         currentNode.add_result(result)
         
         #Adding the moves from current game to tree
+        color = "White"
         for move in game.mainline_moves():
-            currentNode = currentNode.add_child(move.uci())
+            currentNode = currentNode.add_child(move.uci(), color)
             currentNode.add_result(result)
+            if color == "White":
+                color = "Black"
+            else:
+                color = "White"
     return treeStart
