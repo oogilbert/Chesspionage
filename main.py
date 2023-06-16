28941f0b6bb17116f264cbda76d024091b30442e
oogilbert <gilbert.oscar@gmail.com>
2023-06-16 10:26:35 -0500

    Format results as percentages

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ white = history.children[0]
 black = history.children[1]
 print("")
 print("Number of games played as white: " + str(white.weight))
-print("Winrate as white:  " + str(100 * white.result / white.weight))
+print("Winrate as white:  " + str(int(100 * white.result / white.weight)) + "%")
 print("")
 print("Number of games played as black: " + str(black.weight))
-print("Winrate as black:  " + str(100 * black.result / black.weight))
+print("Winrate as black:  " + str(int(100 * black.result / black.weight)) + "%")
 user_color = input("enter \"White\" to inspect games as white or \"Black\" to inspect games as black: ")
-if user_color == "White":
+if user_color == "White" or user_color == "white":
     history = white
-elif user_color == "Black":
+elif user_color == "Black" or user_color == "black":
     history = black
 else:
     raise Exception("Error: unrecognized color")
@@ -35,7 +35,7 @@ while history.children != []:
         print("")
         print(node.color + " plays: " + node.move)
         print("Number of games played:  " + str(node.weight))
-        print("Winrate:  " + str(100 * node.result / node.weight))
+        print("Winrate:  " + str(int(100 * node.result / node.weight)) + "%")
     next_move = input("Enter next move: ")
     for node in history.children:
         if node.move == next_move:
